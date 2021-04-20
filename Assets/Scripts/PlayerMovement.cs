ff7a96296ab5c11fcad79bb27649ef9f27f96630
TinuviaWV <71916978+Tinuvia@users.noreply.github.com>
2021-04-20 19:39:44 +0200

    adds falling leaves

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,45 +4,54 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float movementSpeed;
     public Rigidbody2D rb;
     public Animator anim;
     public float jumpForce;
+    public float movementSpeed;
     public Transform feetBox;
     public LayerMask groundLayers;
-
-
 
     float _mx;
     bool _jumpRequest;
 
 
-    void Update() {
+    void Update()
+    {
         _mx = Input.GetAxisRaw("Horizontal");
         bool bGrounded = Physics2D.OverlapCircle(feetBox.position, 0.5f, groundLayers);
 
-        if (Input.GetButtonDown("Jump") && bGrounded) {
+        if (Input.GetButtonDown("Jump") && bGrounded)
+        {
             anim.SetTrigger("TakeOff");
             _jumpRequest = true;
         }
 
-        if(Mathf.Abs(_mx) > 0.01f) {
+        if (Mathf.Abs(_mx) > 0.01f)
+        {
             transform.localScale = new Vector3(Mathf.Sign(_mx), 1f, 1f); // flips the player if needed
             anim.SetBool("IsWalking", true);
-        } else {
+        }
+        else
+        {
             anim.SetBool("IsWalking", false);
         }
 
         anim.SetBool("IsGrounded", bGrounded);
     }
 
-    private void FixedUpdate() {
-        // move (use AddForce instead)
+    private void FixedUpdate()
+    {
+        // move (Mass = 1, JumpForce = 17, movementSpeed = 10) 
         Vector2 movement = new Vector2(_mx * movementSpeed, rb.velocity.y);
         rb.velocity = movement;
 
+        /* For an ice level?  Mass = 1, JumpForce = 15, movementSpeed = 35 
+        Vector2 movement = new Vector2(_mx * movementFSpeed, 0);
+        rb.AddForce(movement);
+        */
+
         // jump
-        if(_jumpRequest)
+        if (_jumpRequest)
         {
             rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
             _jumpRequest = false;
